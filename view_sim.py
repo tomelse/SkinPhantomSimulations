5fb8db9d99495a9120c491376512bb534daecdb5
Tom Else <thomas.else@cruk.cam.ac.uk>
2020-04-21 16:41:36 +0100

    UNITS

---
+++ view_sim.py
@@ -0,0 +1,9 @@
+from pymcx import read_output_file, load_mcx_settings
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 0_1_09
+result = read_output_file("Exp_demo/result_0_1_09.mc2", load_mcx_settings("Exp_demo/settings_0_1_09.json"))
+
+plt.imshow(np.log(np.squeeze(result[:,100,:])))
+plt.show()
