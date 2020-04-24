7b8545c45da1a89602e176c9baee81688ccd9935
Tom Else <thomas.else@cruk.cam.ac.uk>
2020-04-24 15:48:59 +0100

    Sim run

--- generate_phantom.py
+++ generate_phantom.py
@@ -82,11 +82,3 @@ run_output = pd.DataFrame(output)
 run_output.to_csv(os.path.join(experiment_folder,"params.csv"))
 run_output.to_html(os.path.join(experiment_folder, "params.html"))
 
-files = glob.glob("Exp_demo/settings_*.json")
-for i, file in enumerate(files):
-    print(file)
-    print(str(int(100 * i / len(files))) + "%")
-    folder, json_file = os.path.split(file)
-    file_stub = "_".join(os.path.splitext(json_file)[0].split("_")[1:])
-    if len(glob.glob(os.path.join(folder, file_stub + ".mc2"))) == 0:
-        os.system("mcxcl -f " + file + " -d 0 -U 0 -r 100")
